4669ff5792f9356889d98e30198b70145a4fc974
Nicholas Bucher <behappy54321@gmail.com>
2022-12-12 16:57:17 -0500

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import { API } from "@stoplight/elements";
-
 function App() {
   return (
     <div className="App">
-      Example...
-      <br />
-      <API apiDescriptionUrl="https://raw.githubusercontent.com/stoplightio/Public-APIs/master/reference/zoom/openapi.yaml" />
+      This is an example empty repo
+      {/* <API apiDescriptionUrl="https://raw.githubusercontent.com/stoplightio/Public-APIs/master/reference/zoom/openapi.yaml" /> */}
     </div>
   );
 }
