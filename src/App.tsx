33ddcfe199733d4c9ee5455a92e69990a275dd02
Nicholas Bucher <behappy54321@gmail.com>
2022-12-12 16:34:10 -0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { API } from "@stoplight/elements";
+
+function App() {
+  return (
+    <div className="App">
+      Example...
+      <br />
+      <API apiDescriptionUrl="https://raw.githubusercontent.com/stoplightio/Public-APIs/master/reference/zoom/openapi.yaml" />
+    </div>
+  );
+}
+
+export default App;
